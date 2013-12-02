8ecf75a777e245c6ad522a3ac3d73cca7c6b57d7
Jeremy Cerise <jcerise06@gmail.com>
2013-12-02 14:49:34 -0700

    Final server revamp

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+
 app.use(express.static('app'));
-app.listen(process.env.PORT);
+
+app.listen(process.env.PORT || 3000);
